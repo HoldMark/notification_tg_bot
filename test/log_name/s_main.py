95d8128b58d28689a59b18e6caee0491a9c8c27c
Mark <prodius.mark@mail.ru>
2024-02-08 23:47:21 +0300

    _

--- test/log_name/s_main.py
+++
@@ -1,9 +0,0 @@
-from log import get_logger
-from s_sub import some_def
-
-# log_main = get_logger(__name__)
-log_main = get_logger()
-
-log_main.info('msg from main file')
-
-some_def()
--- test/log_name/s_sub.py
+++
@@ -1,8 +0,0 @@
-from log import get_logger
-
-log_def = get_logger()
-
-
-def some_def():
-    log_def.info('msg from sub file')
-    pass

d91c8a3cc147308012e856d2863c9e0980d162c1
Mark <prodius.mark@mail.ru>
2024-02-10 18:28:16 +0300

    rename commands and change description

--- src/core/commands.py
+++ src/core/commands.py
@@ -5,9 +5,9 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(command='start', description='Начало работы'),
-        BotCommand(command='set_today', description='Установить список уведомлений'),
-        BotCommand(command='remove_today', description='Не отправлять сегодня уведомления'),
-        BotCommand(command='reset_today', description='Переустановить уведомления'),
+        BotCommand(command='set', description='Установить уведомления'),
+        BotCommand(command='remove', description='Убрать уведомления'),
+        BotCommand(command='reset', description='Переустановить уведомления'),
         BotCommand(command='get_list', description='Получить список уведомлений'),
         BotCommand(command='prolong', description='Продлить на n часов')
     ]
--- src/core/dispetcher.py
+++ src/core/dispetcher.py
@@ -15,9 +15,9 @@ dp = Dispatcher()
 
 dp.message.register(start_command, Command('start'))
 dp.message.register(get_notification_list, Command('get_list'))
-dp.message.register(set_ntf_today, Command('set_today'))
-dp.message.register(reset_msgs_today, Command('reset_today'))
-dp.message.register(remove_msgs_today, Command('remove_today'))
+dp.message.register(set_ntf_today, Command('set'))
+dp.message.register(reset_msgs_today, Command('reset'))
+dp.message.register(remove_msgs_today, Command('remove'))
 dp.message.register(prolong, Command('prolong'))
 dp.message.register(get_hours, ProlongState.GET_HOUR)
 

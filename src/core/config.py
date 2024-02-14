1ec369181c3d4ac781a8169c99afd31cb44fe5b1
Mark <prodius.mark@mail.ru>
2024-02-14 22:25:36 +0300

    _

--- src/core/config.py
+++ src/core/config.py
@@ -6,5 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 TG_USER_ID = os.environ.get("TG_USER_ID")
 
+# TODO поправить base_dir
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOGS_DIR = os.path.join(BASE_DIR, '../../logs')

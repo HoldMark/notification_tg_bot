cc0aa1f4fd8c0cc7b3acc632331e845b425f43c6
Mark <prodius.mark@mail.ru>
2024-02-23 13:58:41 +0300

    fix typing

--- src/core/config.py
+++ src/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-TG_USER_ID = os.environ.get("TG_USER_ID")
+TG_USER_ID = int(os.environ.get("TG_USER_ID"))
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.join(CURRENT_DIR, '../../')
